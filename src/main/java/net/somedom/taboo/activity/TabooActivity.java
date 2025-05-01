55e90bae1f924016115b1ace7856e013790c29cc
SomeDom <2009ddominik@gmail.com>
2025-05-01 23:11:30 +0200

    TabooActivity

--- src/main/java/net/somedom/taboo/activity/TabooActivity.java
+++ src/main/java/net/somedom/taboo/activity/TabooActivity.java
@@ -3,6 +3,7 @@ package net.somedom.taboo.activity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -19,19 +20,13 @@ public class TabooActivity {
     @SubscribeEvent
     public static void onEntityDeath(LivingDeathEvent event) {
         if (event.getEntity().level() instanceof ServerLevel serverLevel && event.getEntity() instanceof Villager) {
-            if (
-                    event.getSource().is(DamageTypes.IN_FIRE) ||
-                    event.getSource().is(DamageTypes.ON_FIRE) ||
-                    event.getSource().is(DamageTypes.CAMPFIRE) ||
-                    event.getSource().is(DamageTypes.LAVA) ||
-                    event.getSource().is(DamageTypes.HOT_FLOOR)
-            ) {
+            if (event.getSource().is(DamageTypes.PLAYER_ATTACK)) {
                 BlockPos deathPos = event.getEntity().blockPosition();
                 ChunkAccess chunk = serverLevel.getChunk(deathPos);
 
                 HashMap<String, Integer> activity = new HashMap<>(chunk.getData(AttachmentRegistry.ACTIVITY));
 
-                activity.put("SPIRIT",activity.getOrDefault("SPIRIT", 0) + 5);
+                activity.put("SPIRIT",activity.getOrDefault("SPIRIT", 0) + 1);
                 chunk.setData(AttachmentRegistry.ACTIVITY, activity);
                 chunk.setUnsaved(true);
             }
