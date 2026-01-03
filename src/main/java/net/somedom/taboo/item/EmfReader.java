6ea356a3c19d789edc3d47fde7a7dc6c4f71294b
SomeDom <2009ddominik@gmail.com>
2026-01-03 03:11:24 +0100

    emf thing

--- src/main/java/net/somedom/taboo/item/EmfReader.java
+++ src/main/java/net/somedom/taboo/item/EmfReader.java
@@ -1,6 +1,15 @@
 package net.somedom.taboo.item;
-
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.somedom.taboo.entity.custom.TangoEntity;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EmfReader extends Item {
 
@@ -8,4 +17,31 @@ public class EmfReader extends Item {
         super(properties);
     }
 
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
+        super.inventoryTick(stack, level, entity, slotId, isSelected);
+
+        if (level instanceof ServerLevel serverLevel) {
+
+            AABB testArea = new AABB(entity.blockPosition()).inflate(32);
+            List<LivingEntity> livingEntities = serverLevel.getEntitiesOfClass(LivingEntity.class, testArea);
+            AtomicInteger emfLevel = new AtomicInteger();
+
+            emfLevel.set(0);
+
+            livingEntities.forEach(e -> {
+
+                if (e instanceof TangoEntity) {
+                    if (e.distanceTo(entity) <= 8) {
+                        emfLevel.set(3);
+                    } else if (e.distanceTo(entity) <= 16) {
+                        emfLevel.set(2);
+                    } else if (e.distanceTo(entity) <= 32) {
+                        emfLevel.set(1);
+                    }
+                }
+            });
+            emfLevel.set(0);
+        }
+    }
 }
